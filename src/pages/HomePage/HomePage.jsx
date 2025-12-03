e72cb38f7eac53f5142a69b28580b9eff84c20e7
Paul Ochieng <akelopauljudeochieng@gmail.com>
2025-12-03 08:56:26 +0300

    feat: update HomePage layout and project details in projects.json for improved presentation and accuracy

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -14,30 +14,7 @@ export function HomePage() {
     return (
         <div className="home-page" style={bgStyle}>
         <title>Paul Akelo</title>
-        {/*
-        <main className="page home">
-            <div className="page-inner">
-            <section className="hero bg-gradient-to-r from-[#bf1cff] to-[#007185]">
-                <div className="hero-text">
-                    <h2>Hello, I'm Paul Akelo</h2>
-                    <p>Aspiring software developer and GIS developer building impacting visuals and insights.</p>
-                    <Link to="/projects" className="btn">View my work</Link>
-                </div>
-            </section>
-
-            <section className="skills">
-                <h3>Key Skills</h3>
-                <div className="skills-grid">
-                    <div className="skill-card">HTML & CSS</div>
-                    <div className="skill-card">JavaScript</div>
-                    <div className="skill-card">Python</div>
-                    <div className="skill-card">SQL</div>
-                </div>
-            </section>
-            </div>
-        </main> */}
-
-         <Hero />
+            <Hero />
         {/*
             <Skills data={skillsData} />
        */}
